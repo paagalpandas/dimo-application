cd13993162b983e2ac30eb54bd890e941837d71c
Hemant Chaudhary <hemant.chaudhary@thoughtworks.com>
2020-01-27 15:58:49 +0530

    [Dashboard] - [Hemant/Sanket] - Added dashboard page with the simple mock layout.

---
+++ dimo-ui/src/app/containers/components/dashboard.component.ts
@@ -0,0 +1,127 @@
+import { Component } from '@angular/core';
+import { ICategory } from '../helpers/dashboard.interfaces';
+
+
+@Component({
+  selector : "dimo-dashboard",
+  templateUrl : '../views/dashboard.component.html',
+  styleUrls : ['../styles/scss/dashboard.component.scss']
+})
+export class DashboardComponent{
+
+   public categories: Array<ICategory> = [
+       {
+         category: 'trending',
+         movies: [{
+           name: "Interstellar",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+         },
+         {
+           name: "PK",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BMTYzOTE2NjkxN15BMl5BanBnXkFtZTgwMDgzMTg0MzE@._V1_SX300.jpg"
+         },
+         {
+           name: "1917",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SX300.jpg"
+         },
+         {
+           name: "Inception",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+         },
+         {
+                    name: "Stree",
+                    description: "A great Movie.",
+                    thumbNail: "https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+                  }]
+       },
+       {
+         category: 'Romantic',
+         movies: [{
+           name: "Interstellar",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+         },
+         {
+           name: "PK",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BMTYzOTE2NjkxN15BMl5BanBnXkFtZTgwMDgzMTg0MzE@._V1_SX300.jpg"
+         },
+         {
+           name: "1917",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SX300.jpg"
+         },
+         {
+           name: "Inception",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+         },
+         {
+                    name: "Stree",
+                    description: "A great Movie.",
+                    thumbNail: "https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+                  }]
+       },
+       {
+         category: 'French',
+         movies: [{
+           name: "Interstellar",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+         },
+         {
+           name: "PK",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BMTYzOTE2NjkxN15BMl5BanBnXkFtZTgwMDgzMTg0MzE@._V1_SX300.jpg"
+         },
+         {
+           name: "1917",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SX300.jpg"
+         },
+         {
+           name: "Inception",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+         },
+         {
+                    name: "Stree",
+                    description: "A great Movie.",
+                    thumbNail: "https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+                  }]
+       },
+       {
+         category: 'Walt Disney',
+         movies: [{
+           name: "Interstellar",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"
+         },
+         {
+           name: "PK",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BMTYzOTE2NjkxN15BMl5BanBnXkFtZTgwMDgzMTg0MzE@._V1_SX300.jpg"
+         },
+         {
+           name: "1917",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SX300.jpg"
+         },
+         {
+           name: "Inception",
+           description: "A great Movie.",
+           thumbNail: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+         },
+         {
+                    name: "Stree",
+                    description: "A great Movie.",
+                    thumbNail: "https://m.media-amazon.com/images/M/MV5BMjk4NGZiMzAtODU1NS00MmQ4LWJiNmQtNWU5ZWU4Y2VmNWI0XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+                  }]
+       }
+     ];
+
+}
---
+++ dimo-ui/src/app/containers/helpers/dashboard.interfaces.ts
@@ -0,0 +1,10 @@
+export interface ICategory {
+  category: string;
+  movies: Array<IMovieData>;
+}
+
+export interface IMovieData {
+  name: string;
+  description: string;
+  thumbNail: string;
+}
