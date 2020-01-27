814229c5ab9c6f334085db235630e64f9505c8a4
Anant Agarwal <12.anant@gmail.com>
2020-01-27 12:27:12 +0530

    <Anant|Paridhi> Initial commit for dimo application project structure

---
+++ dimo-services/src/main/java/org/pagalpandas/DimoServicesApplication.java
@@ -0,0 +1,13 @@
+package org.pagalpandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimoServicesApplication.class, args);
+	}
+
+}
---
+++ dimo-services/src/test/java/org/pagalpandas/dimoservices/DimoServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pagalpandas.dimoservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimoServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
