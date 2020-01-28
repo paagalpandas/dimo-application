15afc4d2102587793eb77d37ae3f39377f0b798c
Anant Agarwal <12.anant@gmail.com>
2020-01-28 12:14:43 +0530

    <Anant> re-created ping controller with tests

--- dimo-services/src/main/java/org/pagalpandas/controller/PingController.java
+++ dimo-services/src/main/java/org/pagalpandas/controller/PingController.java
@@ -1,4 +1,16 @@
 package org.pagalpandas.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
 public class PingController {
+
+    @GetMapping("ping")
+    public String ping(){
+        return "hello, pandas!";
+    }
+
 }
