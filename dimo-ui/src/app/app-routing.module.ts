d0d9225100e39970af7c981f8bb809e0cfa3d004
Sandeep Maurya <maurya@dimo.com>
2020-01-29 17:36:00 +0530

    [Manju|Sandeep] Merging from master.

--- dimo-ui/src/app/app-routing.module.ts
+++ dimo-ui/src/app/app-routing.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './containers/components/dashboard.component';
-import { LoginComponent } from "./containers/components/login.component";
+import { EntryPointComponent } from "./containers/components/entrypoint.component";
 import { SearchResultsComponent } from './containers/components/searchresults.component';
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-
-import {DashboardComponent} from './containers/components/dashboard.component';
-import {EntryPointComponent} from "./containers/components/entrypoint.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/entrypoint', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'entrypoint', component: EntryPointComponent}
+  { path: '', redirectTo: '/entrypoint', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'entrypoint', component: EntryPointComponent },
   { path: 'searchresults', component: SearchResultsComponent }
 ];
 
--- dimo-ui/src/app/app.module.ts
+++ dimo-ui/src/app/app.module.ts
@@ -1,25 +1,7 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-
-import {DashboardComponent} from './containers/components/dashboard.component';
-import {LoginComponent} from "./containers/components/login.component";
-import {EntryPointComponent} from "./containers/components/entrypoint.component";
-import {SignUpComponent} from "./containers/components/signup.component";
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {AppRoutingModule} from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -31,9 +13,10 @@ import { SearchboxComponent } from '../app/containers/components/searchbox.compo
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './containers/components/dashboard.component';
+import { EntryPointComponent } from "./containers/components/entrypoint.component";
 import { LoginComponent } from "./containers/components/login.component";
 import { SearchResultsComponent } from "./containers/components/searchresults.component";
-
+import { SignUpComponent } from "./containers/components/signup.component";
 
 @NgModule({
   declarations: [
@@ -41,7 +24,7 @@ import { SearchResultsComponent } from "./containers/components/searchresults.co
     DashboardComponent,
     LoginComponent,
     SearchboxComponent,
-    SearchResultsComponent
+    SearchResultsComponent,
     EntryPointComponent,
     SignUpComponent
   ],
