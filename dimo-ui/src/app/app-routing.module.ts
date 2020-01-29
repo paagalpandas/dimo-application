d0b61da614fa74ed4b3fa7af2bfd6b2f76538f90
Sandeep Maurya <maurya@dimo.com>
2020-01-29 17:18:56 +0530

    [Manju|Sandeep] Search box and results component implementation.

--- dimo-ui/src/app/app-routing.module.ts
+++ dimo-ui/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-
-import {DashboardComponent} from './containers/components/dashboard.component';
-import {LoginComponent} from "./containers/components/login.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './containers/components/dashboard.component';
+import { LoginComponent } from "./containers/components/login.component";
+import { SearchResultsComponent } from './containers/components/searchresults.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'login', component: LoginComponent}
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'searchresults', component: SearchResultsComponent }
 ];
 
 @NgModule({
