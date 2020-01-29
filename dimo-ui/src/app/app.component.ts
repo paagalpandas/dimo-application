c5e6a4e1bf76652700edbb5b9869f30088aedb50
Sandeep Maurya <maurya@dimo.com>
2020-01-29 17:18:56 +0530

    [Manju|Sandeep] Search box and results component implementation.

--- dimo-ui/src/app/app.component.ts
+++ dimo-ui/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { SearchboxComponent } from '../app/containers/components/searchbox.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'DimoUi';
 }
