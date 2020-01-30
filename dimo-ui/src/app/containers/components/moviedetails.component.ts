fbcd28f30ea06057687558f3f8c16b79aabac2e0
Sanket Barapatre <sanket.barapatre@thoughtworks.com>
2020-01-30 21:35:39 +0530

    Issue fix

--- dimo-ui/src/app/containers/components/moviedetails.component.ts
+++ dimo-ui/src/app/containers/components/moviedetails.component.ts
@@ -27,13 +27,8 @@ export class MovieDetailsComponent implements OnInit {
   }
 
   private doSearch(id: string) {
-    let header = new HttpHeaders();
-    // this.profileService.setToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzYW5rZXRAZ21haWwuY29tIiwiRmlyc3ROYW1lIjoiU2Fua2V0IiwiaXNzIjoiRGltb0FwcGxpY2F0aW9uIiwiQXV0aG9yaXRpZXMiOiJST0xFX1ZJRVdFUiIsIkxhc3ROYW1lIjoiIEJhcmFwYXRyZSIsImV4cCI6MTU4MTI1OTM0OH0.0l_QvmUEo1XBK-EWLi57IBBR3KIpUb7g9vNFkfCxkzNIFrap0oWSq62_ZFXgWEnyhNptMA4HdP94t79-hz_LmQ")
-    header = header.append('Authorization', 'Bearer ' + this.profileService.getToken());
-
     let url="/api/movie/"+id;
-
-    this.http.get(url, { headers: header }).subscribe(data => {
+    this.http.get(url).subscribe(data => {
       let response=data as ResponseDTO;
       let movieDetails= (response && response.data) ? response.data as unknown as IMovieDetailsDTO : null;
       console.log(movieDetails);
